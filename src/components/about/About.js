85600275e31a0156dbdc9415d4bac348b89eab56
ParisaDarkhal <p.darkhal@gmail.com>
2023-06-06 00:38:58 -0400

    about me added

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -5,21 +5,39 @@ import AboutCard from "./AboutCard";
 import Icon from "@mdi/react";
 import { mdiElectronFramework } from "@mdi/js";
 
-const cardInfo = [
+const aboutCardInfo = [
   {
     icon: <Icon path={mdiElectronFramework} size={5} />,
-    title: "title 1",
-    text: "text",
+    title: "Love to Learn",
+    text: "eager to take on new challenges and learn new skills",
   },
   {
     icon: <Icon path={mdiElectronFramework} size={5} />,
-    title: "title 1",
-    text: "text",
+    title: "Reliable",
+    text: "Have a strong work ethic & a sense of responsibility",
   },
   {
     icon: <Icon path={mdiElectronFramework} size={5} />,
-    title: "title 1",
-    text: "text",
+    title: "Team Work",
+    text: "team player, always collaborating & cooperating to get the best results",
+  },
+];
+
+const ProjectCardInfo = [
+  {
+    icon: <Icon path={mdiElectronFramework} size={5} />,
+    title: "Fast",
+    text: "Fast load times and lag free interaction",
+  },
+  {
+    icon: <Icon path={mdiElectronFramework} size={5} />,
+    title: "Responsive",
+    text: "Layouts that work on any device, big or small.",
+  },
+  {
+    icon: <Icon path={mdiElectronFramework} size={5} />,
+    title: "Dynamic",
+    text: " Love making pages come to life.",
   },
 ];
 
@@ -34,7 +52,20 @@ export default function About() {
           direction={{ sm: "column", md: "row" }}
           // spacing={{ sm: 6, md: 6 }}
         >
-          {cardInfo.map((item) => (
+          {aboutCardInfo.map((item) => (
+            <AboutCard title={item.title} icon={item.icon} text={item.text} />
+          ))}
+        </Stack>
+      </Box>
+      <Box padding={5} margin="auto">
+        <Typography variant="h3" gutterBottom>
+          I Want My Projects To Be:
+        </Typography>
+        <Stack
+          direction={{ sm: "column", md: "row" }}
+          // spacing={{ sm: 6, md: 6 }}
+        >
+          {ProjectCardInfo.map((item) => (
             <AboutCard title={item.title} icon={item.icon} text={item.text} />
           ))}
         </Stack>
