3562d93c329a66f725184bb40c7900dc0eedf467
ParisaDarkhal <p.darkhal@gmail.com>
2023-06-07 22:57:04 -0400

    Skills edited

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,32 +1,72 @@
 import React from "react";
-import { Box, Stack, Typography, Grid } from "@mui/material";
+import {
+  Box,
+  Stack,
+  Typography,
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+} from "@mui/material";
 import "../styles.css";
 
 export default function Skills() {
   return (
     <Box className="darkBg">
       <Grid container spacing={3}>
-        <Grid item className="skills" sm={12} md={6}>
+        {/* <Grid item className="skills" sm={12} md={6}>
           <img src="./images/Skills.svg" alt="skills" height={450} />
+        </Grid> */}
+        <Grid item sm={12} md={6} className="whoIsMe">
+          <Card
+            sx={{ maxWidth: 450, backgroundColor: "#3d3e49", color: "white" }}
+          >
+            <CardMedia
+              sx={{ height: 350 }}
+              image="./images/Skills.svg"
+              title="ProImage"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Parisa's Programming Skills
+              </Typography>
+              <Typography variant="body2" color="white" className="aboutMe">
+                I'm a web developer with expertise in JavaScript, Node.js, HTML,
+                React, MongoDB, MySQL, Material UI, CSS, Bootstrap, Tailwind,
+                and Handlebars. My skills encompass dynamic web app development,
+                intuitive UI design, and efficient data storage. I'm committed
+                to continuous learning and staying up-to-date with the latest
+                industry trends. My goal is to create visually appealing and
+                user-friendly websites by leveraging powerful technologies and
+                frameworks.
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
         <Grid item sm={12} md={6} className="whoIsMe">
-          <img
-            src="./images/fullPic.svg"
-            alt="Parisa"
-            height={250}
-            className="proImg"
-          />
-          <Typography variant="h5" className="aboutMe">
-            Who is Parisa
-          </Typography>
-          <Typography variant="p" className="aboutMe">
-            I transitioned from a successful career as a Pharmacist and Pharma
-            scientist to pursue my passion for creating and making. Recognizing
-            the vast impact of websites in the daily lives of people worldwide,
-            I made a deliberate choice to enter the field of Web Development.
-            Now, I actively contribute to crafting and delivering user-friendly
-            websites that serve diverse audiences globally.
-          </Typography>
+          <Card
+            sx={{ maxWidth: 450, backgroundColor: "#3d3e49", color: "white" }}
+          >
+            <CardMedia
+              sx={{ height: 350 }}
+              image="./images/fullPic.svg"
+              title="ProImage"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Who is Parisa?
+              </Typography>
+              <Typography variant="body2" color="white" className="aboutMe">
+                I transitioned from a successful career as a Pharmacist and
+                Pharma scientist to pursue my passion for creating and making.
+                Recognizing the vast impact of websites in the daily lives of
+                people worldwide, I made a deliberate choice to enter the field
+                of Web Development. Now, I actively contribute to crafting and
+                delivering user-friendly websites that serve diverse audiences
+                globally.
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
     </Box>
